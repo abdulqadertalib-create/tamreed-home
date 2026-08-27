3508588401cd49b7c49000833dd565a3b2aceaf4
abdulqadertalib-create <abdulqadertalib@gmail.com>
2026-08-28 02:48:00 +0300

    Refactor build.gradle.kts by cleaning up comments
    Removed comments and adjusted formatting in build.gradle.kts.

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -5,15 +5,12 @@ plugins {
 
 android {
     namespace = "iq.tamreed.home"
-
     compileSdk = 35
 
     defaultConfig {
         applicationId = "iq.tamreed.home"
-
         minSdk = 26
         targetSdk = 35
-
         versionCode = 1
         versionName = "1.0"
     }
@@ -35,12 +32,8 @@ android {
 }
 
 dependencies {
-
-    // Android
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-
-    // Material Design
     implementation("com.google.android.material:material:1.12.0")
 
     // Supabase
@@ -48,6 +41,6 @@ dependencies {
     implementation("io.github.jan-tennert.supabase:auth-kt")
     implementation("io.github.jan-tennert.supabase:postgrest-kt")
 
-    // Ktor - Android
+    // Ktor
     implementation("io.ktor:ktor-client-android:3.0.3")
 }
