96063a08840a0e3a657cebb6bbae0667318086ab
abdulqadertalib-create <abdulqadertalib@gmail.com>
2026-08-28 02:45:12 +0300

    Update build.gradle.kts

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -5,11 +5,13 @@ plugins {
 
 android {
     namespace = "iq.tamreed.home"
+
     compileSdk = 35
 
     defaultConfig {
         applicationId = "iq.tamreed.home"
-        minSdk = 24
+
+        minSdk = 26
         targetSdk = 35
 
         versionCode = 1
@@ -33,7 +35,19 @@ android {
 }
 
 dependencies {
+
+    // Android
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
+
+    // Material Design
     implementation("com.google.android.material:material:1.12.0")
+
+    // Supabase
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.5.0"))
+    implementation("io.github.jan-tennert.supabase:auth-kt")
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+
+    // Ktor - Android
+    implementation("io.ktor:ktor-client-android:3.0.3")
 }
