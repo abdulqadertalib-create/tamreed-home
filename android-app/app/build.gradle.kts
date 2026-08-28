d3495d881541178cd70507f005cc3da336cd5cbd
abdulqadertalib-create <abdulqadertalib@gmail.com>
2026-08-28 03:18:27 +0300

    Update compileSdk and targetSdk to version 36

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -5,12 +5,13 @@ plugins {
 
 android {
     namespace = "iq.tamreed.home"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "iq.tamreed.home"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
+
         versionCode = 1
         versionName = "1.0"
     }
@@ -32,12 +33,13 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
 
     // Supabase
-    implementation(platform("io.github.jan-tennert.supabase:bom:3.5.0"))
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.2.3"))
     implementation("io.github.jan-tennert.supabase:auth-kt")
     implementation("io.github.jan-tennert.supabase:postgrest-kt")
 
