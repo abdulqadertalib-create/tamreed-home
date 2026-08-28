64d2be0a4eb95a35c92fa198a11bb320cb36b717
abdulqadertalib-create <abdulqadertalib@gmail.com>
2026-08-28 03:25:05 +0300

    Update SupabaseClient.kt

--- android-app/app/src/main/java/iq/tamreed/home/SupabaseClient.kt
+++ android-app/app/src/main/java/iq/tamreed/home/SupabaseClient.kt
@@ -7,15 +7,9 @@ import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseManager {
 
-    private const val SUPABASE_URL =
-        "https://pmjmfeymnahpockjjafn.supabase.co"
-
-    private const val SUPABASE_KEY =
-        "sb_publishable_HtMExFgxiFq_qhN2I9V76w_0YFG6L0j"
-
     val client: SupabaseClient = createSupabaseClient(
-        supabaseUrl = SUPABASE_URL,
-        supabaseKey = SUPABASE_KEY
+        supabaseUrl = "https://pmjmfeymnahpockjjafn.supabase.co",
+        supabaseKey = "sb_publishable_HtMExFgxiFq_qhN2I9V76w_0YFG6L0j"
     ) {
         install(Auth)
         install(Postgrest)
