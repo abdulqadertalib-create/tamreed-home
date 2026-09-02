7d5f0e067271c36aaffb7661a58207b42b4af465
abdulqadertalib-create <abdulqadertalib@gmail.com>
2026-09-02 13:36:39 +0300

    Add Google Services plugin to build.gradle.kts

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -2,4 +2,7 @@ plugins {
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0" apply false
+
+    // Firebase / Google Services
+    id("com.google.gms.google-services") version "4.5.0" apply false
 }
