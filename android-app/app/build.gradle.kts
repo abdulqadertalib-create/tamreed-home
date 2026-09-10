58e9b19a60dfe89dde45a993a9328bf4759caf17
abdulqadertalib-create <abdulqadertalib@gmail.com>
2026-09-11 02:44:22 +0300

    Add Supabase storage dependency to build.gradle.kts

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(platform("io.github.jan-tennert.supabase:bom:3.2.3"))
     implementation("io.github.jan-tennert.supabase:auth-kt")
     implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.github.jan-tennert.supabase:storage-kt")
 
     // Ktor
     implementation("io.ktor:ktor-client-android:3.0.3")
