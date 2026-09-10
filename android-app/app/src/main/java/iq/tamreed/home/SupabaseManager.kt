3a682432734e8fa631289ce6b64718da5dfb6c0c
abdulqadertalib-create <abdulqadertalib@gmail.com>
2026-09-11 02:40:36 +0300

    Add Storage installation to SupabaseManager

--- android-app/app/src/main/java/iq/tamreed/home/SupabaseManager.kt
+++ android-app/app/src/main/java/iq/tamreed/home/SupabaseManager.kt
@@ -4,6 +4,7 @@ import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.storage.Storage
 
 object SupabaseManager {
 
@@ -30,5 +31,6 @@ object SupabaseManager {
             }
 
             install(Postgrest)
+            install(Storage)
         }
 }
