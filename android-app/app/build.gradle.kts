81c647d072be1c44cae2f678c378a44c238c0fb8
abdulqadertalib-create <abdulqadertalib@gmail.com>
2026-09-13 17:01:50 +0300

    Update build.gradle.kts

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -29,6 +29,12 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
 
         debug {
