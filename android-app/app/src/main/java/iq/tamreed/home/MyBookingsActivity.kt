21adbaa010fadd2f890bb2b84a2a4851d20dd7d5
abdulqadertalib-create <abdulqadertalib@gmail.com>
2026-09-19 13:03:51 +0300

    Add Supabase auth and postgrest imports

--- android-app/app/src/main/java/iq/tamreed/home/MyBookingsActivity.kt
+++ android-app/app/src/main/java/iq/tamreed/home/MyBookingsActivity.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.postgrest.from
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
